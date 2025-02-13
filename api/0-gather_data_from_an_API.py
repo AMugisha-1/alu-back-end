9bf4ee39a0a714f1245d4382a3cfc88c96345060
AMugisha-1 <a.mugisha1@alustudent.com>
2025-02-13 03:30:54 -0800

    000-g

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-"""Module"""
+"""Module to fetch user task completion details"""
 
 import requests
 import sys
 
-
-"""Module"""
-
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: {} <user_id>".format(sys.argv[0]))
+        sys.exit(1)
+
     user_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(user_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(user_id)
+    
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
 
-    user_info = requests.request('GET', user_url).json()
-    todos_info = requests.request('GET', todos_url).json()
+    # Fetch user information
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
 
-    employee_name = user_info["name"]
-    task_completed = list(filter(lambda obj:
-                                 (obj["completed"] is True), todos_info))
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Invalid user ID or failed API request.")
+        sys.exit(1)
+
+    user_info = user_response.json()
+    todos_info = todos_response.json()
+
+    employee_name = user_info.get("name", "Unknown")
+    task_completed = [task for task in todos_info if task.get("completed")]
+
     number_of_done_tasks = len(task_completed)
     total_number_of_tasks = len(todos_info)
 
-    print("Employee {} is done with tasks({}/{}):".
-          format(employee_name, number_of_done_tasks, total_number_of_tasks))
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_number_of_tasks}):")
 
-    [print("\t " + task["title"]) for task in task_completed]
+    for task in task_completed:
+        print(f"\t {task['title']}")
